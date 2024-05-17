dd1a38d0873bd81c99b5cfba5168d99f49959d3c
Kevin DeMarco <kevin@kevindemarco.com>
2024-05-17 17:28:12 -0400

    name instead of hardware_plugin_name in humble

--- gz_ros2_control/src/gz_ros2_control_plugin.cpp
+++ gz_ros2_control/src/gz_ros2_control_plugin.cpp
@@ -426,7 +426,7 @@ void GazeboSimROS2ControlPlugin::Configure(
   }
 
   for (unsigned int i = 0; i < control_hardware_info.size(); ++i) {
-    std::string robot_hw_sim_type_str_ = control_hardware_info[i].hardware_plugin_name;
+    std::string robot_hw_sim_type_str_ = control_hardware_info[i].name;
     RCLCPP_INFO(
       this->dataPtr->node_->get_logger(), "Load hardware interface %s ...",
       robot_hw_sim_type_str_.c_str());
